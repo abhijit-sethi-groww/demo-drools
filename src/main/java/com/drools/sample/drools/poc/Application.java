68bb055367865fc5fd87ddefb3b790888f7d937f
abhijit-sethi-groww <abhijit.sethi@groww.in>
2023-12-06 13:36:39 +0530

    progress in attemp 2

--- src/main/java/com/drools/sample/drools/poc/Application.java
+++ src/main/java/com/drools/sample/drools/poc/Application.java
@@ -3,7 +3,7 @@ package com.drools.sample.drools.poc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.drools.sample")
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/drools/sample/drools/poc/service/impl/FileUploadHandlerServiceImpl.java
+++ src/main/java/com/drools/sample/drools/poc/service/impl/FileUploadHandlerServiceImpl.java
@@ -1,31 +1,32 @@
 package com.drools.sample.drools.poc.service.impl;
 
 import com.drools.sample.drools.poc.dto.FileUploadDto;
-import com.drools.sample.drools.poc.enums.FileFormatStatus;
 import com.drools.sample.drools.poc.service.FileUploadHandlerService;
 import lombok.extern.slf4j.Slf4j;
-import org.kie.api.KieServices;
-import org.kie.api.builder.KieBuilder;
-import org.kie.api.builder.KieFileSystem;
-import org.kie.api.builder.KieRepository;
-import org.kie.api.builder.ReleaseId;
-import org.kie.api.io.Resource;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
-import org.kie.internal.io.ResourceFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.List;
 
 @Service
 @Slf4j
 public class FileUploadHandlerServiceImpl implements FileUploadHandlerService {
 
+    @Autowired
+    private KieContainer kieContainer;
+
+    public void parseFileUploadDto(FileUploadDto fileUploadDto) {
+        KieSession kieSession = kieContainer.newKieSession();
+        kieSession.setGlobal("fileUploadDto", fileUploadDto);
+        kieSession.fireAllRules();
+        kieSession.dispose();
+    }
     @Override
     public void handleFileUpload() {
         log.info("I am here");
         FileUploadDto fileUploadDto = FileUploadDto.builder().fileName("CAN YOU PARSE THIS").numRows(100).build();
+        parseFileUploadDto(fileUploadDto);
+        log.info("{} parsed response",fileUploadDto);
     }
 }
